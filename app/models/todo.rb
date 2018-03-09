411bdbfb0a9b90332a38abde1e8a604e72d2cfe6
Chris Mark <chris.mark@peakdesign.com>
2018-03-09 09:31:56 -0800

    Create tests and create model associations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,11 @@
 class Todo < ApplicationRecord
+  
+  # associate it with items
+  has_many :items, dependent: :destroy
+  
+  #validations
+  
+  validates_presence_of :title, :created_by
+  
+  
 end
